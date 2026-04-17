cf8e09bf5eedddbdbfbe60f0ae14c2fb007fbbee
lebomorojele <lebomorojele@gmail.com>
2026-04-17 22:26:38 +0200

    Initial Commit

---
+++ Yapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yapper
+//
+//  Created by Lebo Morojele on 2026/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
